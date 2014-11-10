dc30f8a8cbf6750456229025b98fae4824176b15
Ido Cohen <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-10 14:52:59 +0000

    Initial Commit

---
+++ Control FUn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Control FUn
+//
+//  Created by Ido Cohen on 10/11/2014.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Control FUn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Control FUn
+//
+//  Created by Ido Cohen on 10/11/2014.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
