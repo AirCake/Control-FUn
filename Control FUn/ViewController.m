1e6423836494aae94094415481f6b7df0f5737ce
AirCake <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-17 17:45:38 +0000

    Control Fun
    Experimenting with Buttons and Views.

--- Control FUn/ViewController.m
+++ Control FUn/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
 
 @end
 
@@ -17,11 +20,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.sliderLabel.text = @"50";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+
+}
+- (IBAction)backgroundTap:(id)sender {
+    [self.nameField resignFirstResponder];
+    [self.numberField resignFirstResponder];
+}
+- (IBAction)sliderChanged:(UISlider *)sender {
+    
+
+int progress = lroundf(sender.value);
+self.sliderLabel.text = [NSString stringWithFormat:@"%d", progress];
+}
+- (IBAction)leftSwitch:(id)sender {
+}
+- (IBAction)rightSwitch:(id)sender {
 }
 
 @end
